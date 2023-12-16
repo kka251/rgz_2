d37877e8376eb533d5c1b2eb5087ae4e61ec563a
kka251 <k.kurilowa2018@yandex.ru>
2023-12-16 15:50:31 +0700

    правки

--- rgz.py
+++ rgz.py
@@ -26,11 +26,11 @@ def main():
     conn = dbConnect()
     cur = conn.cursor(cursor_factory=extras.DictCursor)
     
-    # Получить все товары
     cur.execute("SELECT * FROM product")
     products = cur.fetchall()
     
     username = session.get("username")
+    
     if not username:
         visibleUser = "Anon"
         return render_template('rgz.html', username=visibleUser, products=products)
@@ -110,49 +110,122 @@ def loginPage():
     
 @rgz.route('/rgz/logout')
 def logout():
-    session.clear()  # Удаление всех полей из сессии
+    session.clear()  
     return redirect('/rgz/log')
+
+# @rgz.route('/rgz/add_to_cart', methods=["POST"])
+# def add_to_cart():
+#     if not session.get("username"):
+#         abort(403)  
+#     product_ids = request.form.getlist("product_id")  
+#     kolvo = request.form.getlist("kolvo")     
+
+#     if not product_ids or not kolvo:
+#         abort(400)
+
+#     conn = dbConnect()
+#     cur = conn.cursor(cursor_factory=extras.DictCursor)
+
+#     cart_items = []
+#     for product_id, kolvo in zip(product_ids, kolvo):
+#         cur.execute("SELECT name_, price, kolvo FROM product WHERE id = %s", (product_id,))
+#         product = cur.fetchone()
+#         if product:
+#             available_kolvo = product["kolvo"]
+#             if available_kolvo >= int(kolvo):
+#                 cart_items.append({"name": product["name_"], "price": product["price"], "kolvo": kolvo})
+#             else:
+#                 cart_items.append({"name": product["name_"], "price": product["price"], "kolvo": available_kolvo})
+        
+
+#     conn.close()
+#     cur.close()
+#     session["cart_items"] = cart_items
+
+#     return render_template("korzina.html", cart_items=cart_items)
+
+# @rgz.route('/rgz/korzina')
+# def cart():
+#     if not session.get("username"):
+#         return redirect('/rgz/login')  # Перенаправление на страницу входа
+
+#     cart_items = session.get("cart_items", [])
+
+#     return render_template("korzina.html", cart_items=cart_items)
 
 @rgz.route('/rgz/add_to_cart', methods=["POST"])
 def add_to_cart():
     if not session.get("username"):
-        abort(403)  
-    product_ids = request.form.getlist("product_id")  # Get a list of product IDs
-    kolvo = request.form.getlist("kolvo")     # Get a list of kolvo
+        abort(403)
+
+    product_ids = request.form.getlist("product_id")
+    kolvo = request.form.getlist("kolvo")
 
     if not product_ids or not kolvo:
         abort(400)
 
-    # Add the products and kolvo to the cart
-    # cart_items = []
-    # for product_id, kolvo in zip(product_ids, kolvo):
-    #     cart_items.append({"product_id": product_id, "kolvo": kolvo})
-
-    # return render_template("korzina.html", cart_items=cart_items)
     conn = dbConnect()
     cur = conn.cursor(cursor_factory=extras.DictCursor)
 
     cart_items = []
+    cart_total = 0  # Переменная для хранения общей суммы
+
     for product_id, kolvo in zip(product_ids, kolvo):
-        cur.execute("SELECT name_, price FROM product WHERE id = %s", (product_id,))
+        cur.execute("SELECT name_, price, kolvo FROM product WHERE id = %s", (product_id,))
         product = cur.fetchone()
+
         if product:
-            cart_items.append({"name": product["name_"], "price": product["price"], "kolvo": kolvo})
+            available_kolvo = product["kolvo"]
+            if available_kolvo >= int(kolvo):
+                item_price = product["price"] * int(kolvo)
+                cart_total += item_price
+                cart_items.append({"name": product["name_"], "price": item_price, "kolvo": kolvo})
+            else:
+                item_price = product["price"] * available_kolvo
+                cart_total += item_price
+                cart_items.append({"name": product["name_"], "price": item_price, "kolvo": available_kolvo})
 
     conn.close()
     cur.close()
     session["cart_items"] = cart_items
+    session["cart_total"] = cart_total
 
-    return render_template("korzina.html", cart_items=cart_items)
+    return render_template("korzina.html", cart_items=cart_items, cart_total=cart_total)
+
+
 @rgz.route('/rgz/korzina')
 def cart():
     if not session.get("username"):
         return redirect('/rgz/login')  # Перенаправление на страницу входа
 
-    # cart_items = request.form.get("product_id")
-
-    # return render_template("korzina.html", cart_items=cart_items)
     cart_items = session.get("cart_items", [])
+    cart_total = session.get("cart_total", 0)
 
-    return render_template("korzina.html", cart_items=cart_items)
+    return render_template("korzina.html", cart_items=cart_items, cart_total=cart_total)
+
+@rgz.route('/rgz/remove_from_cart', methods=["POST"])
+def remove_from_cart():
+    if not session.get("username"):
+        abort(403)
+
+    product_name = request.form.get("product_name")
+    product_price = request.form.get("product_price")
+    product_kolvo = request.form.get("product_kolvo")
+
+    if not (product_name and product_price and product_kolvo):
+        abort(400)
+    cart_items = session.get("cart_items", [])
+    cart_total = session.get("cart_total", 0)
+
+    updated_cart_items = []
+
+    for item in cart_items:
+        if item["name"] == product_name and item["price"] == product_price and item["kolvo"] == product_kolvo:
+            updated_cart_total = item["price"]
+        else:
+            updated_cart_items.append(item)
+
+    session["cart_items"] = updated_cart_items
+
+    return redirect("/rgz/korzina")
 
